fccf5a15248b0c699725b73cde97c20d3a247e89
fatma amelia <fatmaaneliaaa143@gmail.com.>
2024-02-29 15:30:25 +0700

    first commit

---
+++ administrator/footer.php
@@ -0,0 +1,17 @@
+<div class="card mt-2 mb-5">
+    <div class="card">
+        <footer class="d-flex justify-content-center border-top mt-3 bg-danger fixed-bottom">
+            <p>&copy; UKK RPL 2024 | FATMA AMELIA</p>
+    </div>
+</div>
+</div>
+</div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
+</script>
+</body>
+
+</html>
---
+++ petugas/index.php
@@ -0,0 +1,64 @@
+<?php
+include "header.php";
+include "navbar.php";
+?>
+
+<body>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            background: url(6.JPG);
+            background-size: cover;
+            background-attachment: fixed;
+            background-position: center;
+            font-family: 'Century Schoolbook';
+        }
+    </style>
+</body>
+<div class="container">
+    <div class="card mt-3">
+        <div class="text-center card bg-danger-subtle text-black">
+            <div class="card-header">
+                SELAMAT DATANG DIHALAMAN PETUGAS
+            </div>
+            <div class="container">
+                <div class=" mt-5">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="card  bg-danger-subtle text-black">
+                                    <div class="card-body">
+                                        Data Barang
+                                        <?php
+                                        include '../koneksi.php';
+                                        $data_produk = mysqli_query($koneksi, "SELECT * FROM produk");
+                                        $jumlah_produk = mysqli_num_rows($data_produk);
+                                        ?>
+                                        <h3><?php echo $jumlah_produk; ?></h3>
+                                        <a href="data_barang.php" class="btn btn-outline-danger btn-sm">Detail</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="card  bg-danger-subtle text-black">
+                                    <div class="card-body">
+                                        Data Pembelian
+                                        <?php
+                                        include '../koneksi.php';
+                                        $data_penjualan = mysqli_query($koneksi, "SELECT * FROM penjualan");
+                                        $jumlah_penjualan = mysqli_num_rows($data_penjualan);
+                                        ?>
+                                        <h3><?php echo $jumlah_penjualan; ?></h3>
+                                        <a href="pembelian.php" class="btn btn-outline-danger btn-sm">Detail</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <?php
+                include "footer.php";
+                ?>
